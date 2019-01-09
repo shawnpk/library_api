51161395a7501eec9f0d0bd279032558680d297e
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-01-09 10:48:13 -0700

    Create authors controller

---
+++ app/controllers/api/v1/authors_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::AuthorsController < ApplicationController
+  before_action :set_author, only: %i[show update destroy]
+
+  def index
+    authors = Author.includes(:books).paginate(page: params[:page])
+
+    render json: authors, status: :ok
+  end
+
+  def show
+    render json: @author, status: :ok
+  end
+
+  def create
+    author = Author.new(author_params)
+
+    if author.save
+      render json: author, status: :created
+    else
+      render json: { error: author.errors }, status: :unprocesable_entity
+    end
+  end
+
+  def update
+    if @author.update(author_params)
+      render json: @author, status: :ok
+    else
+      render json: { error: author.errors }, status: :unprocesable_entity
+    end
+  end
+
+  def destroy
+    @author.destroy
+
+    head :no_content
+  end
+
+  private
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  scope module: :v1 do
-    resources :authors, except: %i[new edit]
-    resources :books, except: %i[new edit]
-    resources :book_copies, except: %i[new edit]
-    resources :users, except: %i[new edit]
+  scope module: :api do
+    namespace :v1 do
+      resources :authors, except: %i[new edit]
+      resources :books, except: %i[new edit]
+      resources :book_copies, except: %i[new edit]
+      resources :users, except: %i[new edit]
+  end
   end
 end
