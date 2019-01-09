8630a5d847badd472c719389f32df0260438405f
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-01-09 11:36:37 -0700

    Add author, book, book copy and user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+authors = (1..20).map do
+  Author.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+end
+puts '20 authors created'
+
+books = (1..70).map do
+  Book.create!(
+    title: Faker::Book.title,
+    author: authors.sample
+  )
+end
+puts '70 books created'
+
+users = (1..10).map do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email
+  )
+end
+puts '10 users created'
+
+(1..300).map do
+  BookCopy.create!(
+    format: rand(1..3),
+    published: Faker::Date.between(10.years.ago, Date.today),
+    book: books.sample,
+    isbn: Faker::Number.number(13)
+  )
+end
+puts '300 book copies created'
