6fb4fef9cd1a5223a882fd7af5096d058ee61bbb
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-01-09 09:59:35 -0700

    Add routes for authors, books, book_copies and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: :v1 do
+    resources :authors, except: %i[new edit]
+    resources :books, except: %i[new edit]
+    resources :book_copies, except: %i[new edit]
+    resources :users, except: %i[new edit]
+  end
 end
